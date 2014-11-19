7c398e8d7ce03c5d5b51aadf8f90ccbd98cfd4bd
Maxim <maxim.shishmarev@monash.edu>
2014-11-19 19:32:36 +1100

    compareNodes method added

--- treecomparison.cpp
+++ treecomparison.cpp
@@ -3,15 +3,20 @@
 void
 TreeComparison::compare(TreeCanvas* t1, TreeCanvas* t2) {
     Node::NodeAllocator* na1 = t1->na;
-    VisualNode* root = (*na1)[0];
-    qDebug() << "kids root: " << root->getNumberOfChildren();
-    VisualNode* kid1 = root->getChild(*na1, 0);
-    VisualNode* kid2 = root->getChild(*na1, 1);
-    qDebug() << "kids node1: " << kid1->getNumberOfChildren();
-    qDebug() << "kids node2: " << kid2->getNumberOfChildren();
-    qDebug() << "status1: " << kid1->getStatus();
-    qDebug() << "status2: " << kid2->getStatus();
+    Node::NodeAllocator* na2 = t2->na;
+    VisualNode* root1 = (*na1)[0];
+    VisualNode* root2 = (*na2)[0];
 
+    qDebug() << TreeComparison::copmareNodes(root1, root2);
+    
 
-    // Node::NodeAllocator* na2 = t2->na;
+}
+
+bool
+TreeComparison::copmareNodes(VisualNode* n1, VisualNode* n2) {
+    if (n1->getNumberOfChildren() != n2->getNumberOfChildren())
+        return false;
+    if (n1->getStatus() != n2->getStatus())
+        return false;
+    return true;
 }
--- treecomparison.hh
+++ treecomparison.hh
@@ -7,6 +7,10 @@ class TreeComparison {
 public:
   static void compare(TreeCanvas* t1, TreeCanvas* t2);
 
+private:
+  /// Return true/false depending on whether n1 ~ n2
+  static bool copmareNodes(VisualNode* n1, VisualNode* n2); /// TODO: make it inline?
+
 };
 
 #endif
