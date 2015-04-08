d67c8ce26c1d5e9b9b3982a228c97cc6ebde1eaf
Chris Mears <chris@cmears.id.au>
2015-04-09 09:38:58 +1000

    Put SubtreeCountCursor implementation in proper file.

--- nodecursor.hpp
+++ nodecursor.hpp
@@ -370,3 +370,64 @@ DisposeCursor::processCurrentNode(void) {
     node()->dispose();
 }
 
+inline
+SubtreeCountCursor::SubtreeCountCursor(VisualNode *theNode,
+                   int _threshold,
+                   const VisualNode::NodeAllocator& na)
+  : NodeCursor<VisualNode>(theNode, na),
+    threshold(_threshold)
+{
+    stack.push_back(0);
+}
+
+inline void
+SubtreeCountCursor::processCurrentNode(void) {
+    stack.back()++;
+    int x = stack.back();
+    VisualNode* n = node();
+    // A threshold of zero means turn this stuff off.
+    if (threshold == 0) {
+        n->setSubtreeSizeUnknown();
+        n->setHidden(false);
+        n->setChildrenLayoutDone(false);
+    } else if (x <= threshold) {
+        n->setHidden(true);
+        n->setChildrenLayoutDone(true);
+        if (x < threshold/4) {
+            n->setSubtreeSize(0);
+        } else if (x < threshold/2) {
+            n->setSubtreeSize(1);
+        } else if (x < 3*threshold/4) {
+            n->setSubtreeSize(2);
+        } else {
+            n->setSubtreeSize(3);
+        }
+    } else {
+        n->setHidden(false);
+        n->setChildrenLayoutDone(false);
+    }
+    n->dirtyUp(na);
+}
+
+inline void
+SubtreeCountCursor::moveSidewards(void) {
+    NodeCursor<VisualNode>::moveSidewards();
+    int x = stack.back();
+    stack.pop_back();
+    stack.back() += x;
+    stack.push_back(0);
+}
+
+inline void
+SubtreeCountCursor::moveUpwards(void) {
+    NodeCursor<VisualNode>::moveUpwards();
+    int x = stack.back();
+    stack.pop_back();
+    stack.back() += x;
+}
+
+inline void
+SubtreeCountCursor::moveDownwards(void) {
+    NodeCursor<VisualNode>::moveDownwards();
+    stack.push_back(0);
+}
