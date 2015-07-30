3df18d644fa6b85d3bfc1bab7d09b4ceb1752f2e
Maxim <maxim.shishmarev@monash.edu>
2015-07-30 10:27:33 +1000

    added license

--- cmp_tree_dialog.cpp
+++ cmp_tree_dialog.cpp
@@ -1,3 +1,7 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
 #include "cmp_tree_dialog.hh"
 #include "nodewidget.hh"
 #include "treecomparison.hh"
